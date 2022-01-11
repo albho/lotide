79c6500f238b1d1e60601a3e39038eb7e9bbd8a8
Albert Ho <albertcyi.ho@gmail.com>
2022-01-10 16:38:25 -0800

    Completed tail function

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  if (typeof array !== "object") return undefined;
+  if (array.length <= 1) return [];
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(words.length, 3);
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(result);
