4da8f42d1aeffdb18d70146d3f071060154509c2
Albert Ho <albertcyi.ho@gmail.com>
2022-01-11 15:15:32 -0800

    Completed flatten function without via iteration

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return console.log(`😁👍 Assertion Passed: ${array1} === ${array2}`);
+  }
+  return console.log(`😓👎 Assertion Failed: ${array1} !== ${array2}`);
+};
+
+const flatten = function (array) {
+  const newArray = [];
+
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      for (let nestedItem of item) {
+        newArray.push(nestedItem);
+      }
+    } else {
+      newArray.push(item);
+    }
+  }
+
+  return newArray;
+};
+
+// => should all PASS
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4], "5", [6]]), [1, 2, 3, 4, "5", 6]);
+assertArraysEqual(flatten([1, 2, ["3", 4], "5", [6]]), [1, 2, "3", 4, "5", 6]);
