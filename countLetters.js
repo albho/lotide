250b8f5f18d6563e47f915fa3b36bddef2c7285e
Albert Ho <albertcyi.ho@gmail.com>
2022-01-12 09:25:45 -0800

    Commented out assertEqual helper function

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,11 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// Unsure about necessity of assertEqual
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const countLetters = function (sentence) {
   const finalCount = {};
