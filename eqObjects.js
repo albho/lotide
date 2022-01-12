2dcb18b7ac074dd4f1cfbd7e9862b0bcbe333b52
Albert Ho <albertcyi.ho@gmail.com>
2022-01-12 15:21:06 -0800

    Updated test to use assertEqual helper function

--- eqObjects.js
+++ eqObjects.js
@@ -22,7 +22,7 @@ const eqObjects = function (object1, object2) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
+assertEqual(eqObjects(ab, abc), false);
