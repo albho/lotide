fe51aae3df9bde60cf89c29b7405d4e73b9c5bbb
Albert Ho <albertcyi.ho@gmail.com>
2022-01-12 15:42:40 -0800

    Fixed for loop syntax

--- eqObjects.js
+++ eqObjects.js
@@ -13,16 +13,27 @@ const eqObjects = function (object1, object2) {
 
   if (allKeys1.length !== allKeys2.length) return false;
 
-  for (let key of allKeys1) {
-    if (allKeys1[key] !== allKeys2[key]) return false;
+  for (let key in allKeys1) {
+    if (allKeys1[key] !== allKeys2[key]) {
+      return false;
+    }
   }
 
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
+// Assertions for primitive values
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true);
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false);
+
+// Assertions for array values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
