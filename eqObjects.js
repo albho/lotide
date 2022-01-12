baab506ece1b9d3ee0641aaf88b7b7928fedff75
Albert Ho <albertcyi.ho@gmail.com>
2022-01-12 10:25:12 -0800

    Completed eqObjects function for object with primitives as values

---
+++ eqObjects.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+const eqObjects = function (object1, object2) {
+  const allKeys1 = Object.keys(object1);
+  const allKeys2 = Object.keys(object2);
+
+  if (allKeys1.length !== allKeys2.length) return false;
+
+  for (let key of allKeys1) {
+    if (allKeys1[key] !== allKeys2[key]) return false;
+  }
+
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
