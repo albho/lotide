0675e6043624f123c3dd56c5c1d3f83d2e89dbbc
Albert Ho <albertcyi.ho@gmail.com>
2022-01-12 09:11:56 -0800

    Completed countLetters function

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const finalCount = {};
+
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (letter in finalCount) {
+        finalCount[letter]++;
+      } else {
+        finalCount[letter] = 1;
+      }
+    }
+  }
+
+  return finalCount;
+};
+
+console.log(countLetters("lighthouse in the house"));
