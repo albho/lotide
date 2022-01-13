cb2389d05c4272490f9306f457ba7f3d4354c952
Albert Ho <albertcyi.ho@gmail.com>
2022-01-12 19:23:46 -0800

    Refactored if...else to use ternary

--- tail.js
+++ tail.js
@@ -1,9 +1,7 @@
 const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
+  actual === expected
+    ? console.log(`😁👍 Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`😓👎 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 const tail = function (array) {
