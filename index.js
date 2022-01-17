bd0182206f986f9eff4484d188847f16f5056979
Albert Ho <albertcyi.ho@gmail.com>
2022-01-17 13:53:45 -0800

    Created single export object for all functions

---
+++ index.js
@@ -0,0 +1,23 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without,
+};
