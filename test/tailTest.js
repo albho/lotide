d362ed92534dadd661a5648ef89d3bee9133fcc8
Albert Ho <albertcyi.ho@gmail.com>
2022-01-17 13:29:02 -0800

    Refactored tests to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,22 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-// => should all PASS
-assertArraysEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-assertArraysEqual(tail(["Hello", "Lighthouse", "Labs"]), [
-  "Lighthouse",
-  "Labs",
-]);
+describe("#tail", () => {
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    const actual = tail([1, 2, 3, 4, 5]);
+    const expected = [2, 3, 4, 5];
+    assert.deepEqual(actual, expected);
+  });
+
+  it("returns 1 for [1, 2, 3]", () => {
+    const actual = tail(["Hello", "Lighthouse", "Labs"]);
+    const expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(actual, expected);
+  });
+
+  it("returns [] for ['5']", () => {
+    const actual = tail(["5"]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+});
