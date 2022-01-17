4f4a407f8f2d8249ce9976c92d918bf9a87fbe23
Albert Ho <albertcyi.ho@gmail.com>
2022-01-17 09:12:03 -0800

    Added test cases

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,8 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5); // pass
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
+assertEqual(head([5, 6, 7]), 6); // fail
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse"); // fail
